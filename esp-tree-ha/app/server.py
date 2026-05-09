e125ba9d83d1c87ec07f901bb540dcaebcab9aca
Ben <ben@example.com>
2026-05-09 23:33:45 +0800

    QuickPush

--- esp-tree-ha/app/server.py
+++ esp-tree-ha/app/server.py
@@ -334,7 +334,7 @@ def create_app() -> FastAPI:
         bridge_manager=bridge_manager,
     )
 
-    app = FastAPI(title="ESP Tree Add-on", version="0.1.144")
+    app = FastAPI(title="ESP Tree Add-on", version="0.1.146")
     app.state._activity_positions = {}
     app.state.settings = settings
     app.state.db = db
